8f17f1a0357f60a09b3f3a5e0fe945790447d891
Faldak <jetikezen@gmail.com>
2026-04-03 21:25:37 +0500

    dryaniki

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from groq import Groq
 from dotenv import load_dotenv
 
 load_dotenv()
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 
 # Configure logging - set to INFO to avoid rate limiting
 logging.basicConfig(level=logging.INFO)
